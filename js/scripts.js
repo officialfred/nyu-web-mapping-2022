35e3874ad112d5bbaee3f42c7492393c9030dc30
officialfred <fl1176@nyu.edu>
2022-02-03 21:21:11 -0300

    first commit after porting jsfiddle to the hub

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+
+
+
+var button1 = document.getElementById("button1");
+var button2 = document.getElementById("button2");
+var button3 = document.getElementById("button3");
+var button4 = document.getElementById("button4");
+var vid = document.getElementById("vid");
+
+
+button1.onclick = function() {
+    button2.style.visibility = "visible";
+    button1.style.visibility = "hidden";
+}
+
+button2.onclick = function() {
+    button3.style.visibility = "visible";
+    button2.style.visibility = "hidden";
+}
+
+button3.onclick = function() {
+    button4.style.visibility = "visible";
+    button3.style.visibility = "hidden";
+}
