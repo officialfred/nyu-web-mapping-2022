279d446ed1f56d8f8e4a200f1b97f87827975101
officialfred <fl1176@nyu.edu>
2022-02-10 17:25:45 -0500

    Clean code, added comments
    Removed some remains from older iterations, added comment

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,15 @@
 
 // need to change this to jquery
 
+// create variables for the buttons to allow them to disappear
 var button1 = document.getElementById("button1");
 var button2 = document.getElementById("button2");
 var button3 = document.getElementById("button3");
-var button4 = document.getElementById("button4");
-var vid = document.getElementById("vid");
 
-
+// create functions for each of the first three buttons. /n
+// They make themselves disappear and show the next button. /n
+// They do the same, but to other buttons /n
+// Next step is to learn how to write this more concisely
 button1.onclick = function() {
     button2.style.visibility = "visible";
     button1.style.visibility = "hidden";
